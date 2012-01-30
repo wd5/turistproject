98ceec2caa8763534d448a27219ef8bf9a59bd88
dfalk <tfalc@mail.ru>
2012-01-30 12:40:06 +0400

    remove old apps

--- callquery/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls.defaults import url, patterns
-
-urlpatterns = patterns('callquery.views',
-
-    url(r'^add/$', view='callquery', name='callquery'),
-
-)
